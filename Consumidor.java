10e53d8d4e7f50fa2d75691492760d01845360d7
Leandro <carlosl.cp@hotmail.com>
2017-04-12 11:41:59 -0300

    Update Consumidor.java

--- Consumidor.java
+++ Consumidor.java
@@ -4,25 +4,25 @@ import java.util.concurrent.BlockingQueue;
 
 public class Consumidor extends Thread {
 	private BlockingQueue<Integer> estoque;
-    private int id;
-		
-    public Consumidor(BlockingQueue<Integer> c, int id) {
-    	estoque = c;
-    	this.id = id;
-    }
+	private int id;
 
-    public void run() {
-        int value = 0;
+	public Consumidor(BlockingQueue<Integer> c, int id) {
+		estoque = c;
+		this.id = id;
+	}
+
+	public void run() {
+		int value = 0;
         
-        for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 5; i++) {
 			try {
 				value = estoque.take();
 				System.out.println("Comsumidor " + id + " consumiu " + value);
 				sleep(2000);
-	        } 
+			} 
 			catch (InterruptedException e) { 
-	        	System.exit(1);
-	        }        
+				System.exit(1);
+			}
 		}
-    }
+	}
 }
